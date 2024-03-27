ce5f49f8b45767af6dfd138dddd3aa7c15ac0cec
Luc Saikali <lbsaikali@cpp.edu>
2024-03-26 19:12:02 -0700

    tried some costmap stuff

--- launch/map.launch.py
+++ launch/map.launch.py
@@ -1,48 +1,22 @@
-import os
-
+from launch import LaunchDescription
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch_ros.actions import Node
-
-
 def generate_launch_description():
+    # Get the package share directory
+    your_package_share_directory = get_package_share_directory('billee_bot')
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    # Define the launch description
+    ld = LaunchDescription()
 
-    package_name = 'billee_bot'  
-
-    rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-            )
-        ]),
+    # Start the costmap node
+    costmap_node = Node(
+        package='nav2_costmap_2d',
+        executable='nav2_costmap_2d',
+        name='costmap_node',
+        output='screen',
+        parameters=[your_package_share_directory + '/config/costmap_params.yaml']
     )
+    ld.add_action(costmap_node)
 
-    robot_localization = Node(package='robot_localization', executable='ukf_node',
-                              parameters=[{'imu0': '/odom/Imu',
-                                           "imu0_config": [False, False, False,
-                                                           True, True, True,
-                                                           False, False, False,
-                                                           True, True, True,
-                                                           True, True, True],
-                                           "publish_tf": True,
-                                           "base_link_frame": "base_link",
-                                           "odom_frame": "odom",
-                                           "world_frame": 'odom'}])
-    
-    cost_map = Node(package="nav2_costmap_2d", executable="nav2_costmap_2d", 
-                    parameters=[{"file": "$(find nav2_costmap_2d)/launch/example/example_params.yaml",
-                                 "command": "load", 
-                                 "ns": "costmap"}])
-
-    return LaunchDescription([
-        rsp,
-        robot_localization,
-    ])
+    return ld
