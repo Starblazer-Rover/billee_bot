5c10cccfb33d0c6c7ed861309003871fc1084625
Luc Saikali <lbsaikali@cpp.edu>
2024-05-24 18:35:13 -0700

    update

--- launch/map.launch.py
+++ launch/map.launch.py
@@ -17,6 +17,46 @@ def generate_launch_description():
         output='screen',
         parameters=[your_package_share_directory + '/config/costmap_params.yaml']
     )
-    ld.add_action(costmap_node)
+
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='robot_localization_node',
+        output='screen',
+        parameters=[{
+                     'odom_frame': 'odom', 
+                     'base_link_frame': 'base_link',
+                     'map_frame': 'map',
+                     'world_frame': 'odom',  
+                     'two_d_mode': True,
+                     'print_diagnostics': True,
+                     'imu0': '/movement/Imu',
+                     'imu0_config': [False, False, False,
+                                     False, False, True,
+                                     False, False, False,
+                                     False, False, False,
+                                     False, False, False],
+                      'odom0': '/odom/Encoder', 
+                      'odom0_config': [True, True, False,
+                                       False, False, True, 
+                                       False, False, False, 
+                                       False, False, False, 
+                                       False, False, False]
+                    }]
+    )
+
+    """ 
+    
+    'imu0': '/movement/Imu',
+                     'imu0_config': [False, False, False,
+                                     True, True, True,
+                                     False, False, False,
+                                     True, True, True,
+                                     True, True, True],"""
+
+    #ld.add_action(costmap_node)
+    ld.add_action(robot_localization_node)
+
+
 
     return ld
